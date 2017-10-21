eaa68bc91d8aa5e45aad56e86a6556c968d438a2
Yongqiang Fan <yf322@drexel.edu>
2017-10-20 21:27:39 -0400

    forget about everything before

--- client.py
+++ client.py
@@ -1,59 +1,27 @@
-import thread
+from threading import Thread
 import threading
 import socket
 import time
-from scapy.all import *
 import sys
+import psutil
 
-dst = sys.argv[1]
-port = int(sys.argv[2])
-pkt = None
-lock = threading.Lock()
 
-def craft(message):
-	global pkt
-	lock.acquire()
-	pkt[TCP].seq += 1
-	pkt[TCP].load = message
-	pkt[TCP].ack = 0
-	del pkt[IP].chksum
-	del pkt[IP].len
-	del pkt[TCP].chksum
-	pkt[TCP].chksum = 72*256 + 105#hex= 0x4869, or “Hi”
-	pkt[TCP].flags = "C" #this is how we identify our packet
-	pkt.show2()
-	lock.release()
-	return pkt
+def messWithCpu():
+	print("Messing with Cpu")
+	timeout = time.time() + 10
+	x = 2
+        while True:
+                x *= x
+		if time.time() > timeout:
+			break
+	threading.Timer(10, messWithCpu).start()
 
-def sniffer():
-	global pkt
-	while True:#change src address if not testing locally
-		p = sniff(filter="tcp and type=0x800 and src=127.0.0.1 and dst=" + dst, count=1, store=1)
-		try:
-			p[0][TCP].seq
-		except:
-			continue
-		lock.acquire()
-		pkt = p[0]
-		lock.release()
 
 def client():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print 'Socket created'
+	print("read from cpu usage")
+	messWithCpu()
+#	for i in range(0,5):
+#		print(psutil.cpu_percent(interval=10))
 
-	s.connect((dst, port))
-	
-	#send 5 packets. Fifth packet is crafted packet
-
-	for i in range(5):
-		s.send(str(i))
-		time.sleep(.1)
-	send(craft('5'))#our crafted packet
-	time.sleep(.1)
-	for i in range(5, 10):#send 5 more packets
-		s.send(str(i))
-		time.sleep(.1)
 if __name__ == "__main__":
-	thread.start_new_thread(sniffer, ())
 	client()
-
--- server.py
+++ server.py
@@ -1,50 +1,13 @@
-import thread
+import psutil
 import threading
-import socket
 import time
-from scapy.all import *
+import socket
 import sys
 
-host = sys.argv[1]
-port = int(sys.argv[2])
-pkt = None
-lock = threading.Lock()
-
-def parse():
-	global pkt
-	try:
-		cs = pkt[TCP].chksum#TCP checksum is where message should be
-	except IndexError as E:
-		return
-	if pkt[TCP].flags == 0x080:#our packet has TCP.flags = "C"
-		print "Aha! A message! " + str(cs)#print TCP checksum
-		exit()
-	return
-
-def sniffer():
-	global pkt
-	while True:
-		p = sniff(count=1, store=1)
-		lock.acquire()
-		pkt = p[0]
-		parse()
-		lock.release()
-
 def server():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print 'Socket created'
-
-	s.bind((host, port))
-
-	s.listen(10)
-	conn, addr = s.accept()
-
-	data = conn.recv(1)
-	while data != "":
-		print data
-		data = conn.recv(1)
-
+	for i in range(0, 5):
+		print(psutil.cpu_percent(interval=10))
+	
 if __name__ == "__main__":
-	thread.start_new_thread(sniffer, ())
 	server()
 
