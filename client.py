defc15960c9cd4340ce130cee52dc2a26cd7a419
Yongqiang Fan <yf322@drexel.edu>
2017-10-24 22:44:07 -0400

    timeout

--- client.py
+++ client.py
@@ -1,20 +1,63 @@
-from threading import Thread
-import threading
+from timeout import timeout
 import time
 import sys
-#import psutil
+import urllib2
+import timeit
 import pyspeedtest
 
-st = pyspeedtest.SpeedTest()
 
-def messWithNetwork():
-	print("messing with network speed")
+class Detecter:
 
-def client():
-	print("read the network status")
-	messWithNetwork()
-	for i in range(0,5):
-		print(st.download())
+    def __init__(self, timeout):
+        self.calibrate_n = 5
+        self.read = ''
+        self.timeout = timeout
+
+    def downloadSpeed(self):
+        url = 'http://speedtest.ftp.otenet.gr/files/test1Mb.db'
+        request = urllib2.Request(url, headers={'User-Agent': "Magic Brw"})
+        response = urllib2.urlopen(request)
+        start = time.time()
+        try:
+            with timeout(seconds=self.timeout - 2):
+                response.read()
+                end = time.time()
+        except:
+            end = start + 8
+        t = float(end - start)
+        return 1024/t, t
+
+    def calibrate(self):
+        print("Testing the network speed for calibrate")
+        result = 0
+        for i in range(0, self.calibrate_n):
+            start = time.time()
+            _, runtime = self.downloadSpeed()
+            result += runtime
+            end = time.time()
+            print "Calibrating #", i, "runtime", end - start
+        return result/float(self.calibrate_n)
+
+    def client(self):
+        print "Read the network status"
+        avg = self.calibrate()
+        print "The average download speed is:", avg, "kb/s"
+        while True:
+            _, runtime = self.downloadSpeed()
+            start = time.time()
+            if (runtime - avg)/avg > 0.15:
+                self.read += '1'
+            else:
+                self.read += '0'
+            print self.read
+            end = time.time()
+            print "Runtime for this round:", runtime + end - start
+            time.sleep(self.timeout - (runtime + end - start))
 
 
-client()
+if __name__ == "__main__":
+    f = open('inputStr', 'r')
+    input_str = f.readline()
+    detecter = Detecter(10)
+    detecter.client()
+
--- server.py
+++ server.py
@@ -1,64 +1,54 @@
-import psutil
-import threading
 import multiprocessing
 import time
-import socket
 import sys
 import urllib2
 import sched
-import libtorrent as lt
+import timeit
 
 class NetworkJam:
-
-	def __init__(self):
+	def __init__(self, timeout):
 		self.threads = []
 		self.s = sched.scheduler(time.time, time.sleep)
+		self.timeout = timeout
 
 	def http(self):
 		#print "start downloading 100mb"
-		url = 'http://ipv4.download.thinkbroadband.com/100MB.zip'
+		url = 'http://ipv4.download.thinkbroadband.com/512MB.zip'
 		request = urllib2.Request(url, headers={'User-Agent': "Magic Brw"})
 		response = urllib2.urlopen(request)
-		html = response.read()
-		self.s.enter(20, 1, sys.exit, ())
-		self.s.run()
-		
-
-	def p2p(self):
-		ses = lt.session()
-		ses.listen_on(6881, 6891)
-
-		e = lt.bdecode(open("test.torrent", 'rb').read())
-		info = lt.torrent_info(e)
-
-		params = { 'save_path': '.', 'storage_mode': lt.storage_mode_t.storage_mode_sparse, 'ti': info }
-		h = ses.add_torrent(params)
-
-		s = h.status()
-		while (not s.is_seeding):
-	        	s = h.status()
-			state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating']
-        		print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state])
-        	time.sleep(1)
+		response.read()
 
 	def shutdown(self):
 		print("shut down")
 		for t in self.threads:
 			t.terminate()
 			t.join()
-		
 
-
+	def sendBits(self, string):
+		n = 50
+		for s in string:
+			if s == '0':
+				print "sending 0"
+				time.sleep(self.timeout)
+			else:
+				start = timeit.default_timer()
+				print "sending 1"
+				for i in range(1, n):
+					t = multiprocessing.Process(target=nj.http)
+					nj.threads.append(t)
+					t.start()
+				end = timeit.default_timer()
+				time.sleep(self.timeout - (end -start))
+				self.shutdown()
 
 if __name__ == "__main__":
-	nj = NetworkJam()
-	n = 50
-	print 'downloading 100mb for', n, 'times'
-	for i in range(1, n):
-		t = multiprocessing.Process(target=nj.http)
-		nj.threads.append(t)
-		t.start()
-	
-	time.sleep(10)
-	nj.shutdown()
+	try:
+		f = open('inputStr', 'r')
+		input_str = f.readline()
+		print input_str
+		nj = NetworkJam(10)
+		nj.sendBits(input_str)
+
+	except KeyboardInterrupt:
+		sys.exit(1)
 
